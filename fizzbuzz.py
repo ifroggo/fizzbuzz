ddcd3eaf354704f8c02426ae25ebb3571145fe2e
ifroggo <64921548+ifroggo@users.noreply.github.com>
2021-10-17 20:23:32 +0100

    ini commit

---
+++ fizzbuzz.py
@@ -0,0 +1,22 @@
+
+
+
+
+def fizzBuzz(number):
+    output = ""
+    if(number % 3 == 0):
+        output += "Fizz"
+    if(number % 5 == 0):
+        output += "Buzz"   
+    if(output == ""):
+        output += str(number)
+    output += ","
+    return output
+
+
+
+if __name__ == '__main__':
+    output = ""
+    for i in range(100):
+        output += fizzBuzz(i)
+    print(output)
